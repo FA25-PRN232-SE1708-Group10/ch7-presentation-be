f11c7fe502445506380ad289f473639cb151cdac
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-09-25 23:11:26 +0700

    fix(cors): change origin to correct port

--- API/Program.cs
+++ API/Program.cs
@@ -30,7 +30,7 @@ namespace API
                     builder =>
                     {
                         builder
-                            .WithOrigins("http://localhost:3000")
+                            .WithOrigins("http://localhost:5173")
                             .AllowAnyHeader()
                             .AllowAnyMethod()
                             .AllowCredentials();
